d22ca9245f6b4fdf0c26acd1af24dc935fee2255
xiongwei.zhu <zhuxiongwei@foxmail.com>
2020-02-03 15:59:10 +0800

    add http request sync

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,5 +1,5 @@
-use async_std::prelude::*;
 use async_std::net::TcpStream;
+use async_std::prelude::*;
 
 #[derive(Debug)]
 pub struct RequestConfig {
@@ -7,7 +7,10 @@ pub struct RequestConfig {
     pub url: &'static str,
 }
 
-pub async fn request(addr: String, config: RequestConfig) -> Result<String, Box<dyn std::error::Error>> {
+pub async fn request(
+    addr: String,
+    config: RequestConfig,
+) -> Result<String, Box<dyn std::error::Error>> {
     let mut stream = TcpStream::connect(addr).await?;
 
     let head_line = format!("{} {} HTTP/1.1\n\n", config.method, config.url);
@@ -16,8 +19,33 @@ pub async fn request(addr: String, config: RequestConfig) -> Result<String, Box<
 
     let mut buffer = Vec::new();
     // FIX: 不知道为什么read_to_end要等好久
+    // 测试了一下，rust tcpStream接收http响
+    // 应的时候，到最后会等待一会儿，看还有没
+    // 有数据没到的，不知道怎么去掉这个等待
     stream.read_to_end(&mut buffer).await?;
 
     let content = String::from_utf8(buffer)?;
     Ok(content)
+}
+
+pub mod sync {
+    use super::RequestConfig;
+    use std::io::prelude::*;
+    use std::net::TcpStream;
+
+    pub fn request(
+        addr: String,
+        config: RequestConfig,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let mut stream = TcpStream::connect(addr)?;
+
+        let head_line = format!("{} {} HTTP/1.1\n\n", config.method, config.url);
+        stream.write(head_line.as_bytes())?;
+
+        let mut buffer = Vec::new();
+        stream.read_to_end(&mut buffer)?;
+        let content = String::from_utf8(buffer)?;
+
+        Ok(content)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use rust_demo::http::*;
 use async_std::task;
+use rust_demo::http::*;
 
 fn main() {
     println!("hello rust!");
@@ -8,6 +8,7 @@ fn main() {
     // test_http();
     // test_http();
     task::block_on(test_http());
+    test_http_sync();
 }
 
 async fn test_http() {
@@ -16,6 +17,17 @@ async fn test_http() {
         url: "/",
     };
     match request(String::from("127.0.0.1:8080"), config).await {
+        Ok(content) => println!("--------------- request Ok ---------------\n{}", content),
+        Err(error) => println!("--------------- request Err ---------------\n{:?}", error),
+    }
+}
+
+fn test_http_sync() {
+    let config = RequestConfig {
+        method: "GET",
+        url: "/",
+    };
+    match sync::request(String::from("127.0.0.1:8080"), config) {
         Ok(content) => println!("--------------- request Ok ---------------\n{}", content),
         Err(error) => println!("--------------- request Err ---------------\n{:?}", error),
     }
